d04fdb986725b9aa53967616ed6f1551305e554e
Dillan <211299820@cput.ac.za>
2015-03-20 10:54:54 +0200

    Basic page  layouts and connection scripts.

---
+++ scripts/ninetythree.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dillan on 3/13/2015.
+ */
+
+function getHeight()
+{
+    var height = window.innerHeight;
+    document.getElementsByClassName('home-page').style.height = height;
+}
---
+++ scripts/scroll.js
@@ -0,0 +1,54 @@
+/* ========================================================================
+ * Ninetythree: scroll.js v1.0.0
+ * Dillan Isaacs
+ * March 8, 2015
+ * https://github.com/studioninetythree/ScrollJS.git
+ * ========================================================================
+ * Licensed under MIT (https://github.com/studioninetythree/ScrollJS/blob/master/README.txt)
+ * ========================================================================
+ * This script works most effectively with Single-Page Sites (SPS's).
+ * It is used to animate auto-scrolling withing the web page using.
+ * Just call the function and pass in the ID of the element you would like the page to scroll to.
+ * The variable 'scrollSpeed' can be increased and decreased to whichever speed you'd like.
+ * */
+
+var scrollSpeed = 24;
+var distance = 80;
+var scrollY;
+
+function scrollTo(el)
+{
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+    var documentHeight = document.body.offsetHeight;
+    var yPosition = currentY + window.innerHeight;
+    var scroller = setTimeout('scrollTo(\''+el+'\')', scrollSpeed);
+
+    if(yPosition > documentHeight)
+    {
+        clearTimeout(scroller);                     // Stops the scroller from scrolling forever
+    }
+    else
+    {
+        if(currentY < (targetY - distance))         // To scroll to contents below the current element
+        {
+            scrollY = currentY + distance;
+            window.scroll(0, scrollY);
+        }
+        else if(currentY > targetY)                 // To scroll to contents above the current element
+        {
+            scrollY = currentY - distance;
+            window.scroll(0, scrollY);
+        }
+        else
+        {
+            clearTimeout(scroller);
+        }
+    }
+    currentY = 0;
+    targetY = 0;
+    documentHeight = 0;
+    yPosition = 0;
+    scrollY = 0;
+
+}
